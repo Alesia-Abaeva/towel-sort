cb318878e6a4f10f0c4e020b5525e7d9575afef8
Alesia <alesia.litvinenko@gmail.com>
2022-08-09 14:06:24 +0300

    fead: sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    
+    if(matrix == undefined){
+        return []
+    }
+    if(matrix.length == 0){
+        return []
+        
+    } else {
+
+    let arrayTowel =  matrix.map((elem => elem.join(' ')))
+
+    for(let i = 1; i< arrayTowel.length; i= i+2){
+        arrayTowel[i] = (arrayTowel[i]).split(' ').reverse().join(' ')
+    
+    }
+
+    let arrayNumbers = (((arrayTowel.join(',')).replace(/\s/g, ',')).split(',')).map(parseFloat)
+    return arrayNumbers}
 }
+
+
+
